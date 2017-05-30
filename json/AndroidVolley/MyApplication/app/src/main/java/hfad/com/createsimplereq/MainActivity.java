cc6f2c3bf096cc66c8b1b6da6bc45e911fe45046
akandMehedi <mehedi.hasan@technodhaka.com>
2017-05-30 11:26:18 +0600

    Update Main

--- json/AndroidVolley/MyApplication/app/src/main/java/hfad/com/createsimplereq/MainActivity.java
+++ json/AndroidVolley/MyApplication/app/src/main/java/hfad/com/createsimplereq/MainActivity.java
@@ -2,15 +2,22 @@ package hfad.com.createsimplereq;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView textView;
     Button button;
+    String server_url = "https://raw.githubusercontent.com/akandMehedi/GIT/master/json/ServerResponse/response.txt";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +27,28 @@ public class MainActivity extends AppCompatActivity {
         textView = (TextView) findViewById(R.id.textView);
         button = (Button) findViewById(R.id.button);
 
-        
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final RequestQueue requestQueue = Volley.newRequestQueue(MainActivity.this);
+
+                StringRequest stringRequest = new StringRequest(Request.Method.POST, server_url,
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                                textView.setText(response);
+                                requestQueue.stop();
+                            }
+                        }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        textView.setText("something going wrong!");
+                        error.printStackTrace();
+                        requestQueue.stop();
+                    }
+                });
+                requestQueue.add(stringRequest);
+            }
+        });
     }
 }
